8dbe7217ea95294e6500204a45efcf7e12a0e7ea
isabell sommerfeld <isi.marie06@gmail.com>
2019-11-11 15:48:30 +0000

    coachanswers

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,23 @@
+class QuestionsController < ApplicationController
+  def ask
+  end
+
+  def answer
+    conversation
+    @answer
+  end
+
+  def conversation
+    @question = params[:question]
+
+    if @question.include?('I am going to work')
+      @answer = 'Great'
+    elsif @question.include?('?')
+      @answer = 'Silly question, get dressed and go to work!'
+    else
+      @answer = 'I dont care, get dressed and go to work!'
+    end
+  end
+end
+
+
